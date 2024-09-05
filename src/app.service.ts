56c9e7a691e4540ab8d6448140b8ebe21220d740
victor duarte <victor.d.dev@gmail.com>
2024-09-05 18:42:15 -0300

    feat: :sparkles: adds prisma config and migrations

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "./prisma/prisma.service";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  async getHello(): Promise<string> {
+
     return "Hello World!";
   }
 }
